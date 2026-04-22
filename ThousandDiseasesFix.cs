670593e6301e8bcb00b9184be540d472bd80ced8
Encore-Un-Nerd <encore.un.nerd@gmail.com>
2026-04-22 13:25:26 +0200

    Fix the issue with the DC falling to 11 after first round when PoA is applied by A Thousand Diseases aura

--- ThousandDiseasesFix.cs
+++ ThousandDiseasesFix.cs
@@ -3,6 +3,7 @@ using BlueprintCore.Utils;
 using Kingmaker.Blueprints;
 using Kingmaker.Designers.EventConditionActionSystem.Actions;
 using Kingmaker.ElementsSystem;
+using Kingmaker.EntitySystem.Stats;
 using Kingmaker.Enums;
 using Kingmaker.RuleSystem;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
@@ -47,6 +48,14 @@ namespace ImprovedPoP
 
             area.AddComponents(rankConfig);
             Logger.Info("Added ContextRankConfig to area effect");
+
+            // After adding the ContextRankConfig, also add ContextCalculateAbilityParamsBasedOnClass
+            var calcParams = new ContextCalculateAbilityParamsBasedOnClass();
+            SetPrivate(calcParams, "m_CharacterClass", 
+                BlueprintTool.GetRef<BlueprintCharacterClassReference>(ShamanClassGuid));
+            calcParams.StatType = StatType.Wisdom;
+            area.AddComponents(calcParams);
+            Logger.Info("Added ContextCalculateAbilityParamsBasedOnClass to area effect");
 
             // Fix DiceType from Zero to One
             var runAction = area.GetComponent<AbilityAreaEffectRunAction>();
